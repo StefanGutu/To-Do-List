14c3b43859932d5d2da49f5e7e43806e4c37d13b
Stefan Gutu <stefangutu66@gmail.com>
2024-07-24 13:03:03 +0300

    changed everything from css to tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import ToDoList from "./ToDoList.jsx";
 
+
 function App(){
 
     return(
         <>
-            <ToDoList/>
+            <ToDoList/>    
         </>
     );
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}', // Adjust the paths according to your project structure
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
   ],
   theme: {
     extend: {},
